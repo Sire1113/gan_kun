04704abfa2a7941d73a06c7c692db901dcf881a4
sire <Sire1113@outlook.com>
2024-11-30 12:31:39 +0800

    reformat the files

--- datasets.py
+++ datasets.py
@@ -4,6 +4,9 @@ import torch.nn.parallel
 import torch.utils.data
 import torchvision.datasets as dset
 import torchvision.transforms as transforms
+import matplotlib.pyplot as plt
+import numpy as np
+import torchvision.utils as vutils
 
 
 def load_data(root: str, image_size: int, batch_size: int) -> torch.utils.data.DataLoader:
@@ -18,3 +21,15 @@ def load_data(root: str, image_size: int, batch_size: int) -> torch.utils.data.D
     dataloader = torch.utils.data.DataLoader(dataset, batch_size=batch_size,
                                              shuffle=True, num_workers=2)
     return dataloader
+
+
+if __name__ == '__main__':
+    # Plot some training images
+    dataloader = load_data(root='./dogs', image_size=64, batch_size=128)
+    real_batch = next(iter(dataloader))
+    plt.figure(figsize=(8, 8))
+    plt.axis("off")
+    plt.title("Training Images")
+    plt.imshow(
+        np.transpose(vutils.make_grid(real_batch[0][:64], padding=2, normalize=True).cpu(), (1, 2, 0)))
+    plt.show()
